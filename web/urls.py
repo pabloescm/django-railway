7de134f711ff993d5bdeb081f3a9bcd38708a8c6
pablo <pabloescm@hotmail.com>
2025-02-04 18:41:37 +1300

    arreglando url de category para view basado en clases

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'category', views.CategoriaViewSet)
+
 urlpatterns = [
-    path('', views.render_articles, name='articles'),
-    path('category/', views.CategoriaViewSet, name='category'),
+    path('', include(router.urls)),
+    path('articles/', views.render_articles, name='articles'),
 ]
